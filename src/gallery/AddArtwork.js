0dbafd81c2a640ee4580579928c2113f57299360
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-06-20 20:05:58 -0500

    updated add artwork form

--- src/gallery/AddArtwork.js
+++ src/gallery/AddArtwork.js
@@ -51,50 +51,69 @@ export default class Gallery extends Component {
         return (
             <div id="add__art__form">
                 <form>
-                    <div className="form-group">
-                        <label htmlFor="title"><h5>Add New artwork</h5></label>
+                    <label htmlFor="title"><h5>Add New artwork</h5></label>
 
+                    <div className="form-group">
                         <input id="title"
                             value={this.state.title}
-                            placeholder="Title"
+                            placeholder="Artwork Title"
                             onChange={this.handleFieldChange}
                             className="form-control"
                             rows="4"></input>
 
-                        <input
-                            id="image_url"
-                            type="file"
-                            label="File"
-                            help="Upload image"
-                            value={this.state.image_url}
-                            onChange={this.handleFieldChange}>
-                        </input>
+                        <div class="input-group mb-3">
+                            <div class="custom-file">
+                                <input
+                                    id="inputGroupFile02"
+                                    type="file"
+                                    label="File"
+                                    help="Upload image"
+                                    className="custom-file-input"
+                                    value={this.state.image_url}
+                                    onChange={this.handleFieldChange}>
+                                </input>
+                                <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
+                            </div>
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="">Upload</span>
+                            </div>
+                        </div>
 
-                        <select id="type"
-                            value={this.state.typeId}
-                            onChange={this.handleFieldChange}
-                            className="form-control"
-                            rows="4">
-                            <option value="Select Type of Artwork">Select Type of Artwork</option>
-                            <option value="Oil Painting">Oil Painting</option>
-                            <option value="Advertising Print">Advertising Print</option>
-                            <option value="Advertising Mockup">Advertising Mockup</option>
-                            <option value="Watercolor Painting">Watercolor Painting</option>
-                        </select>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Type of Artwork</label>
+                            </div>
+                            <select
+                                id="type"
+                                value={this.state.typeId}
+                                onChange={this.handleFieldChange}
+                                className="custom-select">
+                                <option selected>Select... </option>
+                                <option value="Oil Painting">Oil Painting</option>
+                                <option value="Advertising Print">Advertising Print</option>
+                                <option value="Advertising Mockup">Advertising Mockup</option>
+                                <option value="Watercolor Painting">Watercolor Painting</option>
+                            </select>
+                        </div>
 
-                        <select id="artist"
-                            value={this.state.artistId}
-                            onChange={this.handleFieldChange}
-                            className="form-control"
-                            rows="4">
-                            <option value="Select Artist">Select Artist</option>
-                            <option value="Luther Johnson">Luther Johnson</option>
-                            <option value="Suzy West">Suzy West</option>
-                        </select>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Artist</label>
+                            </div>
+                            <select id="artist"
+                                value={this.state.artistId}
+                                onChange={this.handleFieldChange}
+                                className="custom-select">
+                                <option value="Select Artist">Select... </option>
+                                <option value="Luther Johnson">Luther Johnson</option>
+                                <option value="Suzy West">Suzy West</option>
+                            </select>
+                        </div>
 
-                        <div>Year Signed</div>
-                        <input type="date"
+                        <input type="number"
+                            min="1900"
                             id="year_signed"
+                            placeholder="Year Signed"
                             value={this.state.year_signed}
                             onChange={this.handleFieldChange}
                             className="form-control"
@@ -119,40 +138,58 @@ export default class Gallery extends Component {
                             rows="4">
                         </input>
 
-                        <div>Framed?</div>
-                        <input type="checkbox">
-                        </input>
+                        <div className="input-group mb-3 art__checkbox">
+                            <div className="input-group-prepend">
+                                <div className="input-group-text">
+                                    <input type="checkbox" aria-label="Checkbox for following text input" />
+                                </div>
+                            </div>
+                            <p type="text" className="form-control art__notes" aria-label="Text input with checkbox"> Framed? </p> 
+                        </div>
 
-                        <select id="condition"
-                            value={this.state.conditionId}
-                            onChange={this.handleFieldChange}
-                            className="form-control"
-                            rows="4">
-                            <option value="Condition of Artwork">Condition of Artwork</option>
-                            <option value="Exellent">Exellent</option>
-                            <option value="Good">Good</option>
-                            <option value="Fair">Fair</option>
-                            <option value="Poor">Poor</option>
-                        </select>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <label className="input-group-text" for="inputGroupSelect01">Condition of Artwork</label>
+                            </div>
+                            <select id="condition"
+                                value={this.state.conditionId}
+                                onChange={this.handleFieldChange}
+                                className="custom-select">
+                                <option selected>Select...</option>
+                                <option value="Exellent">Exellent</option>
+                                <option value="Good">Good</option>
+                                <option value="Fair">Fair</option>
+                                <option value="Poor">Poor</option>
+                            </select>
+                        </div>
 
-                        <select id="owner"
-                            value={this.state.ownerId}
-                            onChange={this.handleFieldChange}
-                            className="form-control"
-                            rows="4">
-                            <option value="Current Owner of Artwork">Current Owner of Artwork</option>
-                            <option value="Kimberly Bird">Kimberly Bird</option>
-                            <option value="Suzy West">Suzy West</option>
-                            <option value="June Call">June Call</option>
-                            <option value="Melissa Barnum">Melissa Barnum</option>
-                        </select>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Current Owner of Artwork</label>
+                            </div>
+                            <select id="owner"
+                                value={this.state.ownerId}
+                                onChange={this.handleFieldChange}
+                                className="custom-select">
+                                <option selected>Select...</option>
+                                <option value="Kimberly Bird">Kimberly Bird</option>
+                                <option value="Suzy West">Suzy West</option>
+                                <option value="June Call">June Call</option>
+                                <option value="Melissa Barnum">Melissa Barnum</option>
+                            </select>
+                        </div>
 
-                        <textarea id="notes"
-                            value={this.state.notes}
-                            placeholder="Notes and/or Summary of Artwork"
-                            onChange={this.handleFieldChange}
-                            className="form-control"
-                            rows="4"></textarea>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Artwork Notes</span>
+                            </div>
+                            <textarea id="notes"
+                                aria-label="Artwork Notes"
+                                value={this.state.notes}
+                                onChange={this.handleFieldChange}
+                                className="form-control art__notes">
+                            </textarea>
+                        </div>
 
                         <button type="button" onClick={this.postNewArtwork} className="btn btn-info btn-lg">Add</button>
                     </div>
