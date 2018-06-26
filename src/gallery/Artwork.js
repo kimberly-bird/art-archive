3bdf3e90efea77cf56cef0075272fa0e55bf02f3
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-06-26 16:16:33 -0500

    updated gallery view with hover effect

--- src/gallery/Artwork.js
+++ src/gallery/Artwork.js
@@ -4,22 +4,25 @@ import "./Artwork.css"
 export default class Artwork extends Component {
     render() {
         return (
-            <div className="card artwork__card">
-                <img className="card-img-top image artwork__image" alt="Card image cap"
-                    src={this.props.artwork.image_url} />
 
-                <div className="overlay">
-                    <div className="card-body details__overlay ">
-                        <h5 className="card-title">{this.props.artwork.title}</h5>
-                        <h6 className="card-subtitle mb-2">Artist Name | {this.props.artwork.year_signed} | {this.props.artwork.location_created}</h6>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="hovereffect">
+                    <img class="img-responsive" alt="Card image cap"
+                        src={this.props.artwork.image_url} />
+                    <div class="overlay">
+                        <h2 className="card-title">{this.props.artwork.title}</h2>
+                        <h2 className="card-subtitle mb-2">Artist Name | {this.props.artwork.year_signed}</h2>
                         <p className="card-text">{this.props.artwork.notes}</p>
-                        <div>
+                        <p>
+                            <button type="button" class="btn btn-secondary"><a href="#">Details</a></button>
+                        </p>
+                        {/* <div>
                             Type: {this.props.artwork.typeId} |
                             Size: {this.props.artwork.size} |
                             Framed?: {this.props.artwork.framed} |
                             Condition: {this.props.artwork.conditionId} |
                             Current Owner: {this.props.artwork.ownerId}
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
